da3431cca5212be5427189bd056d7c962777c369
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2024-05-28 22:08:07 +0300

    Make rendering concurrent

--- src/dom.js
+++ src/dom.js
@@ -1,19 +1,69 @@
 import { PROP_CHILDREN, TEXT_ELEMENT } from "./jsx.js";
+import { Fiber, createRootFiber } from "./fiber.js";
+import { beginWorkLoop, queueTask } from "./scheduler.js";
 
-export function render(element, container) {
+function createDom(fiber) {
 	const dom =
-		element.type === TEXT_ELEMENT
+		fiber.type === TEXT_ELEMENT
 			? document.createTextNode("")
-			: document.createElement(element.type);
+			: document.createElement(fiber.type);
 
-	for (const [propName, propValue] of Object.entries(element.props)) {
+	for (const [propName, propValue] of Object.entries(fiber.props)) {
 		if (propName === PROP_CHILDREN) continue;
 		dom[propName] = propValue;
 	}
 
-	for (const child of element.props.children) {
-		render(child, dom);
+	return dom;
+}
+
+export function render(element, container) {
+	queueTask(
+		createRootFiber(container, {
+			children: [element],
+		}),
+	);
+	beginWorkLoop(renderUnit, () => {});
+}
+
+function renderUnit(fiber) {
+	fiber.dom ??= createDom(fiber);
+	fiber.parent?.dom.appendChild(fiber.dom);
+
+	const childrenElements = fiber.props.children;
+	let idx = 0;
+	let prevSibling = null;
+
+	while (idx < childrenElements.length) {
+		const element = childrenElements[idx];
+
+		const newFiber = new Fiber(element.type, element.props, null, fiber);
+
+		if (idx === 0) {
+			// First child element becomes child of the fiber
+			fiber.child = newFiber;
+		} else {
+			// The rest become sibling of the previous fiber
+			prevSibling.sibling = newFiber;
+		}
+
+		prevSibling = newFiber;
+		++idx;
 	}
 
-	container.appendChild(dom);
+	if (fiber.child) {
+		// Traverse to child
+		return fiber.child;
+	}
+
+	let nextFiber = fiber;
+
+	while (nextFiber) {
+		if (nextFiber.sibling) {
+			// Then the child's siblings
+			return nextFiber.sibling;
+		}
+
+		// The back to parent and its siblings
+		nextFiber = nextFiber.parent;
+	}
 }
