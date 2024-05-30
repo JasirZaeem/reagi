2e546c2fc31fe45e79850598061f23c48c6d94d6
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2024-05-30 04:21:51 +0300

    Make setState referentially stable

--- src/hooks/useState.js
+++ src/hooks/useState.js
@@ -4,6 +4,25 @@ import { queueTask } from "../scheduler.js";
 
 export const STATE_HOOK = "useState";
 
+function createSetState(setStateContext) {
+	function setState(updater) {
+		this.hook.actionQueue.push(updater);
+		// All state updates from one component are batched to happen during next rerender
+		if (this.fiber.isRerenderQueued) return;
+		queueTask(
+			createRootFiber(
+				renderState.currentRoot.dom,
+				renderState.currentRoot.props,
+				renderState.currentRoot,
+			),
+		);
+
+		this.fiber.isRerenderQueued = true;
+	}
+
+	return setState.bind(setStateContext);
+}
+
 export function useState(initial) {
 	const fiber = renderState.currentFiber;
 	const oldHook = fiber?.alternate?.hooks[renderState.hookIdx];
@@ -11,16 +30,29 @@ export function useState(initial) {
 	const hook = {
 		tag: STATE_HOOK,
 		state: undefined,
+		setStateContext: undefined,
+		setState: undefined,
 		actionQueue: [],
 	};
 
 	if (oldHook) {
 		hook.state = oldHook.state;
-	} else if (initial instanceof Function) {
-		// If an initialiser function is giver, obtain state from it
-		hook.state = initial();
+		hook.setStateContext = oldHook.setStateContext;
+		hook.setStateContext.hook = hook;
+		hook.setStateContext.fiber = fiber;
+		hook.setState = oldHook.setState;
 	} else {
-		hook.state = initial;
+		if (initial instanceof Function) {
+			// If an initialiser function is given, obtain state from it
+			hook.state = initial();
+		} else {
+			hook.state = initial;
+		}
+		hook.setStateContext = {
+			hook,
+			fiber,
+		};
+		hook.setState = createSetState(hook.setStateContext);
 	}
 
 	// Perform all state updates while this component is being rendered
@@ -33,22 +65,7 @@ export function useState(initial) {
 		}
 	}
 
-	function setState(updater) {
-		hook.actionQueue.push(updater);
-		// All state updates from one component are batched to happen during next rerender
-		if (fiber.isRerenderQueued) return;
-		queueTask(
-			createRootFiber(
-				renderState.currentRoot.dom,
-				renderState.currentRoot.props,
-				renderState.currentRoot,
-			),
-		);
-
-		fiber.isRerenderQueued = true;
-	}
-
 	fiber.hooks.push(hook);
 	++renderState.hookIdx;
-	return [hook.state, setState];
+	return [hook.state, hook.setState];
 }
